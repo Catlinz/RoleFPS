4d36c9a262cb0f0b6dce7187400e233beafbe6b3
Catlin Zilinski <catlin@rooof.com>
2019-09-24 09:08:22 -0700

    Initial commit

---
+++ Source/RoleFPS/RoleFPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RoleFPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROLEFPS_API ARoleFPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
